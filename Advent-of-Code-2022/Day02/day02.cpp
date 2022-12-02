7f30e280ac9c4a608dcb39c6f2363f21b69d5ee6
MrSnoodle01 <richardsonjosh03@gmail.com>
2022-12-02 10:17:35 -0800

    cleaned up day 2

--- Advent-of-Code-2022/Day02/day02.cpp
+++ Advent-of-Code-2022/Day02/day02.cpp
@@ -1,98 +1,71 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include <vector>
+#include <unordered_map>
+
+void part1(std::vector<std::string>);
+void part2(std::vector<std::string>);
 
 int main(){
 
     std::ifstream myfile("input.txt");
-
     std::string line;
+    std::vector<std::string> lines;
 
-    std::vector<char> opponent;
-    std::vector<char> me;
-
+    //had to use temp var
+    //some weird character at end of line kept fucking up the hashmap
+    std::string temp;
     while(std::getline(myfile, line)){
-        opponent.push_back(line[0]);
-        me.push_back(line[2]);
+        temp = "";
+        temp += line[0];
+        temp += line[1];
+        temp += line[2];
+        lines.push_back(temp);
     }
 
-    int score = 0;
-    for(int i = 0; i < opponent.size(); i++){
-        // FOR PART 1
-        // if(me[i] == 'X'){
-        //     score += 1;
-        // }else if(me[i] == 'Y'){
-        //     score += 2;
-        // }else{
-        //     score += 3;
-        // }
-
-        // FOR PART 1
-        // switch(opponent[i]){
-        //     case 'A':
-        //         if(me[i] == 'X'){
-        //             score += 3;
-        //             break;
-        //         }else if(me[i] == 'Y'){
-        //             score += 6;
-        //         }
-        //         break;
-        //     case 'B':
-        //         if(me[i] == 'Y'){
-        //             score += 3;
-        //             break;
-        //         }else if(me[i] == 'Z'){
-        //             score += 6;
-        //         }
-        //         break;
-        //     case 'C':
-        //         if(me[i] == 'Z'){
-        //             score += 3;
-        //             break;
-        //         }else if(me[i] == 'X'){
-        //             score += 6;
-        //         }
-        //         break;
-        // }
-
-        // FOR PART 2
-        // switch(opponent[i]){
-        //     case 'A':
-        //         if(me[i] == 'X'){
-        //             score += 3;
-        //             break;
-        //         }else if(me[i] == 'Y'){
-        //             score += 4;
-        //             break;
-        //         }
-        //         score += 8;
-        //         break;
-        //     case 'B':
-        //         if(me[i] == 'Y'){
-        //             score += 5;
-        //             break;
-        //         }else if(me[i] == 'Z'){
-        //             score += 9;
-        //             break;
-        //         }
-        //         score += 1;
-        //         break;
-        //     case 'C':
-        //         if(me[i] == 'Z'){
-        //             score += 7;
-        //             break;
-        //         }else if(me[i] == 'X'){
-        //             score += 2;
-        //             break;
-        //         }
-        //         score += 6;
-        //         break;
-        // }
-    }
-
-    std::cout << score << std::endl;
+    part1(lines);
+    part2(lines);
 
     return 0;
+}
+
+void part1(std::vector<std::string> lines){
+    std::unordered_map<std::string, int> map;
+    map["A X"] = 4;
+    map["A Y"] = 8;
+    map["A Z"] = 3;
+    map["B Y"] = 5;
+    map["B Z"] = 9;
+    map["B X"] = 1;
+    map["C Z"] = 6;
+    map["C X"] = 7;
+    map["C Y"] = 2;
+
+    int final = 0;
+    for(int i = 0; i < lines.size(); i++){
+        final += map[lines[i]];
+    }
+
+    std::cout << "part 1: " << final << std::endl;
+}
+
+void part2(std::vector<std::string> lines){
+    std::unordered_map<std::string, int> map;
+    map["A X"] = 3;
+    map["A Y"] = 4;
+    map["A Z"] = 8;
+    map["B Y"] = 5;
+    map["B Z"] = 9;
+    map["B X"] = 1;
+    map["C Z"] = 7;
+    map["C X"] = 2;
+    map["C Y"] = 6;
+
+    int final = 0;
+    for(int i = 0; i < lines.size(); i++){
+        final += map[lines[i]];
+    }
+
+    std::cout << "part 2: " << final << std::endl;
 }
