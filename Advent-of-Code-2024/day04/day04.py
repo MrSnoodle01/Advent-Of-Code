74a710e3c1a09ec2a20434d3c0707c13c9297e68
Josh <richardsonjosh03@gmail.com>
2024-12-03 22:48:21 -0800

    used austins much cleaner solutions instead

--- Advent-of-Code-2024/day04/day04.py
+++ Advent-of-Code-2024/day04/day04.py
@@ -1,4 +1,15 @@
 # part 1
+wordSearchDirs = [
+  [0, 1],
+  [1, 0],
+  [1, 1],
+  [-1, 0],
+  [0, -1],
+  [-1, -1],
+  [1, -1],
+  [-1, 1],
+]
+
 inputArr = []
 with open('input.txt', 'r') as file:
     for line in file:
@@ -9,42 +20,31 @@ for i in range(len(inputArr)):
     iLen = len(inputArr)
     for j in range(len(inputArr[i])):
         jLen = len(inputArr[i])
-        if(inputArr[i][j] == 'X'):
-            if(i+1 < iLen and inputArr[i+1][j] == 'M'):
-                if(i+2 < iLen and inputArr[i+2][j] == 'A'):
-                    if(i+3 < iLen and inputArr[i+3][j] == 'S'):
-                        final = final + 1
-            if(i-1 >= 0 and inputArr[i-1][j] == 'M'):
-                if(i-2 >= 0 and inputArr[i-2][j] == 'A'):
-                    if(i-3 >= 0 and inputArr[i-3][j] == 'S'):
-                        final = final + 1
-            if(j+1 < jLen and inputArr[i][j+1] == 'M'):
-                if(j+2 < jLen and inputArr[i][j+2] == 'A'):
-                    if(j+3 < jLen and inputArr[i][j+3] == 'S'):
-                        final = final + 1
-            if(j-1 >= 0 and inputArr[i][j-1] == 'M'):
-                if(j-2 >= 0 and inputArr[i][j-2] == 'A'):
-                    if(j-3 >= 0 and inputArr[i][j-3] == 'S'):
-                        final = final + 1
-            if(j-1 >= 0 and i-1 >= 0 and inputArr[i-1][j-1] == 'M'):
-                if(j-2 >= 0 and i-2 >= 0 and inputArr[i-2][j-2] == 'A'):
-                    if(j-3 >= 0 and i-3 >= 0 and inputArr[i-3][j-3] == 'S'):
-                        final = final + 1
-            if(j+1 < jLen and i-1 >= 0 and inputArr[i-1][j+1] == 'M'):
-                if(j+2 < jLen and i-2 >= 0 and inputArr[i-2][j+2] == 'A'):
-                    if(j+3 < jLen and i-3 >= 0 and inputArr[i-3][j+3] == 'S'):
-                        final = final + 1
-            if(j+1 < jLen and i+1 < iLen and inputArr[i+1][j+1] == 'M'):
-                if(j+2 < jLen and i+2 < iLen and inputArr[i+2][j+2] == 'A'):
-                    if(j+3 < jLen and i+3 < iLen and inputArr[i+3][j+3] == 'S'):
-                        final = final + 1
-            if(j-1 >= 0 and i+1 < iLen and inputArr[i+1][j-1] == 'M'):
-                if(j-2 >= 0 and i+2 < iLen and inputArr[i+2][j-2] == 'A'):
-                    if(j-3 >= 0 and i+3 < iLen and inputArr[i+3][j-3] == 'S'):
-                        final = final + 1
+        for di, dj in wordSearchDirs:
+            word = ""
+            ci = i
+            cj = j
+            while ci >= 0 and ci < iLen and cj >= 0 and cj < jLen and len(word) < 4:
+                word += inputArr[ci][cj]
+                if(word == "XMAS"):
+                    final += 1
+                ci += di
+                cj += dj
         
 print("part 1 final: ", final)
+
 # part 2
+topLeftDiag = [
+  [-1, -1],
+  [0, 0],
+  [1, 1],
+]
+topRightDiag = [
+  [1, -1],
+  [0, 0],
+  [-1, 1],
+]
+
 inputArr = []
 with open('input.txt', 'r') as file:
     for line in file:
@@ -56,29 +56,17 @@ for i in range(len(inputArr)):
     for j in range(len(inputArr[i])):
         jLen = len(inputArr[i])
         if(inputArr[i][j] == 'A'):
-            # s on bottoom, m on top
-            if(i+1 < iLen and j+1 < jLen and inputArr[i+1][j+1] == 'S'):
-                if(i-1 >= 0 and j-1 >= 0 and inputArr[i-1][j-1] == 'M'):
-                    if(i+1 < iLen and j-1 >= 0 and inputArr[i+1][j-1] == 'M'):
-                        if(i-1 >= 0 and j+1 < jLen and inputArr[i-1][j+1] == 'S'):
-                            final = final + 1
-            # m on bottom, s on top
-            if(i+1 < iLen and j+1 < jLen and inputArr[i+1][j+1] == 'M'):
-                if(i-1 >= 0 and j-1 >= 0 and inputArr[i-1][j-1] == 'S'):
-                    if(i+1 < iLen and j-1 >= 0 and inputArr[i+1][j-1] == 'S'):
-                        if(i-1 >= 0 and j+1 < jLen and inputArr[i-1][j+1] == 'M'):
-                            final = final + 1
-            # m on left, s on right
-            if(i+1 < iLen and j+1 < jLen and inputArr[i+1][j+1] == 'S'):
-                if(i-1 >= 0 and j-1 >= 0 and inputArr[i-1][j-1] == 'M'):
-                    if(i+1 < iLen and j-1 >= 0 and inputArr[i+1][j-1] == 'S'):
-                        if(i-1 >= 0 and j+1 < jLen and inputArr[i-1][j+1] == 'M'):
-                            final = final + 1
-            # m on right, s on left
-            if(i+1 < iLen and j+1 < jLen and inputArr[i+1][j+1] == 'M'):
-                if(i-1 >= 0 and j-1 >= 0 and inputArr[i-1][j-1] == 'S'):
-                    if(i+1 < iLen and j-1 >= 0 and inputArr[i+1][j-1] == 'M'):
-                        if(i-1 >= 0 and j+1 < jLen and inputArr[i-1][j+1] == 'S'):
-                            final = final + 1
-
+            topLeftWord = ""
+            toprightWord = ""
+            for di, dj in topLeftDiag:
+                ci = i + di
+                cj = j + dj
+                topLeftWord += inputArr[ci][cj] if ci >= 0 and ci < iLen and cj >= 0 and cj < jLen else ""
+            for di, dj in topRightDiag:
+                ci = i + di
+                cj = j + dj
+                toprightWord += inputArr[ci][cj] if ci >= 0 and ci < iLen and cj >= 0 and cj < jLen else ""
+            if((topLeftWord == "MAS" or topLeftWord == "SAM") and (toprightWord == "MAS" or toprightWord == "SAM")):
+                final += 1
+                
 print("part 2 final: ", final)
